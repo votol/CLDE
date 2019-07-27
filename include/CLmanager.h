b05cd1202892f2eca676a710516f1e66ef4c17f6
Vitaliy Martynov <votolm@gmail.com>
2019-07-27 22:53:24 +0300

    configurable opencl manager is added

---
+++ include/CLmanager.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CLmanagerBase.h"
+#include "yaml-cpp/yaml.h"
+
+class CLmanager: public CLmanagerBase
+{
+public:
+    CLmanager(YAML::Node&&);
+    virtual ~CLmanager() override = default;
+    virtual const cl_device_id& device() override;
+    virtual const cl_context& context() override;
+};
---
+++ src/CLmanager.cpp
@@ -0,0 +1,61 @@
+#include "CLmanager.h"
+#include <string>
+
+CLmanager::CLmanager(YAML::Node&& config)
+{
+    cl_device_type device_type;
+    int device_num;
+    if (YAML::Node type = config["cl_device_type"])
+    {
+        std::string tmp_device_type =  type.as<std::string>();
+
+        if(tmp_device_type == "GPU")
+            device_type = CL_DEVICE_TYPE_GPU;
+        else if(tmp_device_type == "CPU")
+            device_type = CL_DEVICE_TYPE_CPU;
+        else
+            device_type = CL_DEVICE_TYPE_GPU;
+    }
+    else
+    {
+        device_type = CL_DEVICE_TYPE_GPU;
+    }
+
+    if(YAML::Node number = config["cl_device_number"])
+    {
+        device_num = number.as<int>();
+    }
+    else
+    {
+        device_num = 0;
+    }
+
+
+    auto devices = getAllDevices(device_type);
+    if(static_cast<size_t>(device_num) >= devices.size())
+        throw std::runtime_error("OpenCL: can't find device");
+
+    auto it = devices.begin();
+    for (int counter = 0; counter < device_num; ++counter)
+    {
+        ++it;
+    }
+    m_device_id = *it;
+
+    cl_int err;
+    cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties>(m_device_id->platform), 0};
+    m_context = clCreateContext(properties, 1, & (device()), nullptr, nullptr, &err);
+    if(err < 0 )
+        throw std::runtime_error("OpenCL: unable to create context");
+}
+
+const cl_device_id& CLmanager::device()
+{
+    return m_device_id->device;
+}
+
+const cl_context& CLmanager::context()
+{
+    return *m_context;
+}
+
